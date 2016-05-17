a53bb68174e18949e95006de5ac7028216bf1c71
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-05-17 16:40:04 -0700

    getting correct starting positions checking inpu

--- rovers.js
+++ rovers.js
@@ -1,3 +1,27 @@
 function roverCom(plateau, start_1, dir_1, start_2, dir_2) {
-	plateau.split(" ")
+	plateau = plateau.split(" ");
+	start_1 = start_1.split(" ");
+
+	var x = 0;
+	var y = 0;
+	var path;
+
+	function start_checker(startingpos) {
+		if(startingpos.length != 3) {
+			return false;
+		} else {
+			x = startingpos[0];
+			y = startingpos[1];
+			path = startingpos[2];
+			return true;
+		}
+	}
+
+	console.log(start_checker(start_1));
+	console.log("x ", x);
+	console.log("x ", y);
+	console.log("path", path);
+
 }
+
+console.log(roverCom("5 5 3 3", "1 2 N", " ", " ", " "));
